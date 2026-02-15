bd91c89432a41b4ea63bd08a7ccf39d606ce2f32
Otavio Fernandes da Silva dos Santos <otaviofernandes113@gmail.com>
2026-02-15 09:59:38 -0300

    feat: add pacote node e arquivo app.js

---
+++ contador-moedas/js/app.js
@@ -0,0 +1,4 @@
+const prompt = require("prompt-sync")();
+console.log("/***** Baú de modedas do Rei *****/\n");
+console.log("-----------------------------------");
+const valInicial = prompt("Digite o valor inicial: ");
