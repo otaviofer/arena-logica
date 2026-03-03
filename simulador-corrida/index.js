e1d20d2f112ef2bffc83ae908c1d3b9d4fce944d
Otavio Fernandes da Silva dos Santos <otaviofernandes113@gmail.com>
2026-03-02 21:55:47 -0300

    refactor: adiciona menu de configuração e motor de corrida dinâmico

--- simulador-corrida/index.js
+++ simulador-corrida/index.js
@@ -3,12 +3,51 @@
 const prompt = require("prompt-sync")();
 
 // --- Configurações Iniciais ---
-const clima = "chuvoso"; 
-const pneu = "macio";
+// const clima = "chuvoso"; 
+// const pneu = "macio";
 let posicaoKM = 0;       // Onde o carro está na pista
-const meta = 50;         // Tamanho da pista
+// const meta = 50;         // Tamanho da pista
 let turnos = 0;
 
+console.log(`\n🏁 Bem-Vindo ao Simulador de Corrida Js!\n`);
+console.log(`\n******* 🏎️🏎️🏎️🏎️🏎️🏎️🏎️🏎️🏎️🏎️ *******\n`);
+
+const escolha = prompt(" Para começar escolha o clima: (1-Sol, 2-Chuva, 3-Tempestade): ");
+let clima; // Esta variável será usada pelos seus IFs lá embaixo
+
+switch (escolha) {
+    case "1":
+        clima = "ensolarado";
+        break;
+    case "2":
+        clima = "chuvoso";
+        break;
+    case "3":
+        clima = "tempestade";
+        break;
+    default:
+        clima = "ensolarado"; // Um valor padrão caso o usuário erre
+        console.log("Opção inválida! Definindo clima como ensolarado por padrão.");
+}
+
+const escolha2 = prompt("\nEscolha o tipo do pneu: (1-Macio, 2-Duro): \n");
+let pneu;
+
+switch (escolha2) {
+    case "1":
+        pneu = "macio";
+        break;
+    case "2":
+        pneu = "duro";
+        break;
+    default:
+        pneu = "duro";
+        console.log("Opção inválida! Definindo tipo do pneu como duro por padrão.");
+}
+
+meta = Number(prompt("Defina o tamanho da pista: "));
+
+console.clear();
 console.log(`\n🏁 Dada a largada! Clima: ${clima} | Pneu: ${pneu}\n`);
 
 // --- A ARENA (Loop de Repetição) ---
